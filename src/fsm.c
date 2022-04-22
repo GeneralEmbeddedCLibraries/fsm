10ffdf461730424ac8c8d51b41da42a4d7a18706
zigam <ziga.miklosic@emsiso.com>
2022-04-22 11:49:57 +0200

    - basic implementation of FSM - needs to be tested

--- src/fsm.c
+++ src/fsm.c
@@ -28,39 +28,74 @@
 // Definitions
 ////////////////////////////////////////////////////////////////////////////////
 
-
+/**
+ * 	FSM States
+ */
+typedef struct
+{
+	uint8_t cur;		/**<Current state */
+	uint8_t next;		/**<Next/Requested state */
+} fsm_state_t;
 
 /**
  * 	FSM data
  */
 typedef struct fsm_s
 {
-	fsm_cfg_t 	cfg;			/**<FSM setup */
-	uint32_t 	duration;		/**<Duration inside FSM state */
+	fsm_cfg_t *	p_cfg;			/**<FSM setup */
+	uint32_t 	loop_cnt;		/**<Loop counter - number of loops inside state */
+	fsm_state_t	state;			/**<Current state of FSM */
 	bool		first_entry;	/**<First entry of state */
+	bool		is_init;		/**<Initialization guard */
 } fsm_t;
+
+/**
+ * 	Limit loop counts
+ *
+ * 	Unit: period of fsm handler
+ */
+#define FSM_LIMIT_LOOP_CNT(cnt)					(( cnt >= 0x1FFFFFFFUL ) ? ( 0x1FFFFFFFUL ) : ( cnt ))
 
 ////////////////////////////////////////////////////////////////////////////////
 // Variables
 ////////////////////////////////////////////////////////////////////////////////
 
-/**
- * 	Initialization guard
- */
-static bool gb_is_init = false;
-
 
 ////////////////////////////////////////////////////////////////////////////////
 // Function Prototypes
 ////////////////////////////////////////////////////////////////////////////////
-
+static void fsm_manager(p_fsm_t fsm_inst);
 
 ////////////////////////////////////////////////////////////////////////////////
 // Functions
 ////////////////////////////////////////////////////////////////////////////////
 
 
+static void fsm_manager(p_fsm_t fsm_inst)
+{
+	// State change
+	if ( fsm_inst->state.cur != fsm_inst->state.next )
+	{
 
+		// TODO:
+		//DRIVE_FSM_DBG_PRINT( "%s -> %s", gs_state_name[ p_fsm -> state.cur ], gs_state_name[ p_fsm -> state.next ] );
+
+		fsm_inst->state.cur 	= fsm_inst->state.next;
+		fsm_inst->first_entry 	= true;
+		fsm_inst->loop_cnt 		= 0UL;
+	}
+
+	// Same state
+	else
+	{
+		fsm_inst->state.next = fsm_inst->state.cur;
+		fsm_inst->first_entry = false;
+
+		// Measure time
+		fsm_inst->loop_cnt += 1UL;
+		fsm_inst->loop_cnt = FSM_LIMIT_LOOP_CNT( fsm_inst->loop_cnt );
+	}
+}
 
 
 
@@ -92,13 +127,29 @@ fsm_status_t fsm_init(p_fsm_t * p_fsm_inst, const fsm_cfg_t * const p_cfg)
 {
 	fsm_status_t status = eFSM_OK;
 
-	if ( false == gb_is_init )
+	if 	(	( NULL != *p_fsm_inst )
+		&&	( NULL != p_cfg ))
 	{
+		if ( false == (*p_fsm_inst)->is_init )
+		{
+			// Get setup
+			(*p_fsm_inst)->p_cfg = (fsm_cfg_t*) p_cfg;
 
+			// Init internal data
+			(*p_fsm_inst)->state.cur = 0;
+			(*p_fsm_inst)->state.next = (*p_fsm_inst)->state.cur;
+			(*p_fsm_inst)->first_entry = false;
+			(*p_fsm_inst)->loop_cnt = 0;
+			(*p_fsm_inst)->is_init = true;
+		}
+		else
+		{
+			status = eFSM_ERROR_INIT;
+		}
 	}
 	else
 	{
-		status = eFSM_ERROR_INIT;
+		status = eFSM_ERROR;
 	}
 
 	return status;
@@ -117,9 +168,9 @@ fsm_status_t fsm_is_init(p_fsm_t fsm_inst, bool * const p_is_init)
 {
 	fsm_status_t status = eFSM_OK;
 
-	if ( NULL != p_is_init )
+	if ( NULL != fsm_inst )
 	{
-		*p_is_init = gb_is_init;
+		*p_is_init = fsm_inst->is_init;
 	}
 	else
 	{
@@ -134,6 +185,110 @@ fsm_status_t fsm_hndl(p_fsm_t fsm_inst)
 {
 	fsm_status_t status = eFSM_OK;
 
+	if ( NULL != fsm_inst )
+	{
+		if ( false == fsm_inst->is_init )
+		{
+			// Manage FSM
+			fsm_manager( fsm_inst );
+
+			// Execute current FSM state
+			if ( NULL != fsm_inst->p_cfg->func[ fsm_inst->state.cur ] )
+			{
+				fsm_inst->p_cfg->func[ fsm_inst->state.cur ]();
+			}
+		}
+		else
+		{
+			status = eFSM_ERROR_INIT;
+		}
+	}
+	else
+	{
+		status = eFSM_ERROR;
+	}
+
+
+	return status;
+}
+
+
+fsm_status_t fsm_goto_state(p_fsm_t fsm_inst, const uint8_t state)
+{
+	fsm_status_t status = eFSM_OK;
+
+	if ( NULL != fsm_inst )
+	{
+		if ( state < fsm_inst->p_cfg->num_of )
+		{
+			fsm_inst->state.next = state;
+		}
+		else
+		{
+			status = eFSM_ERROR;
+		}
+	}
+	else
+	{
+		status = eFSM_ERROR;
+	}
+
+
+	return status;
+}
+
+
+fsm_status_t fsm_get_state(p_fsm_t fsm_inst, uint8_t * const p_state)
+{
+	fsm_status_t status = eFSM_OK;
+
+	if 	(	( NULL != fsm_inst )
+		&& 	( NULL != p_state ))
+	{
+		*p_state = fsm_inst->state.cur;
+	}
+	else
+	{
+		status = eFSM_ERROR;
+	}
+
+
+	return status;
+}
+
+
+fsm_status_t fsm_get_duration(p_fsm_t fsm_inst, uint32_t * const p_loop_cnt)
+{
+	fsm_status_t status = eFSM_OK;
+
+	if 	(	( NULL != fsm_inst )
+		&& 	( NULL != p_loop_cnt ))
+	{
+		*p_loop_cnt = fsm_inst->loop_cnt;
+	}
+	else
+	{
+		status = eFSM_ERROR;
+	}
+
+
+	return status;
+}
+
+
+fsm_status_t fsm_get_first_entry(p_fsm_t fsm_inst, bool * const p_first_entry)
+{
+	fsm_status_t status = eFSM_OK;
+
+	if 	(	( NULL != fsm_inst )
+		&& 	( NULL != p_first_entry ))
+	{
+		*p_first_entry = fsm_inst->first_entry;
+	}
+	else
+	{
+		status = eFSM_ERROR;
+	}
 
 
 	return status;
--- src/fsm.h
+++ src/fsm.h
@@ -52,19 +52,13 @@ typedef enum
  */
 typedef void (*pf_state_t)(void);
 
-typedef struct
-{
-	pf_state_t 	func;		/**<Pointer to function to execute inside state */
-	uint8_t		state;		/**<Corresponding state number to that function */
-} fsm_state_cfg_t;
-
 /**
  * 	FSM Configuration table
  */
 typedef struct
 {
-	fsm_state_cfg_t p_state[8];		/**<Pointer to set of state and its functions */
-	uint8_t	 		num_of;			/**<Number of all states */
+	pf_state_t	func[8];	/**<Pointer to function to execute inside state */
+	uint8_t	 	num_of;		/**<Number of all states */
 } fsm_cfg_t;
 
 /**
@@ -79,8 +73,8 @@ fsm_status_t fsm_init   			(p_fsm_t * p_fsm_inst, const fsm_cfg_t * const p_cfg)
 fsm_status_t fsm_is_init			(p_fsm_t fsm_inst, bool * const p_is_init);
 fsm_status_t fsm_hndl				(p_fsm_t fsm_inst);
 fsm_status_t fsm_goto_state			(p_fsm_t fsm_inst, const uint8_t state);
-fsm_status_t fsm_get_state			(p_fsm_t fsm_inst, uint8_t * const state);
-fsm_status_t fsm_get_duration		(p_fsm_t fsm_inst, float32_t * const p_duration);
+fsm_status_t fsm_get_state			(p_fsm_t fsm_inst, uint8_t * const p_state);
+fsm_status_t fsm_get_duration		(p_fsm_t fsm_inst, uint32_t * const p_loop_cnt);
 fsm_status_t fsm_get_first_entry	(p_fsm_t fsm_inst, bool * const p_first_entry);
 
 
